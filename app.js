94f56e6e724450e59da87b81dd0a96008ef7a132
William Whitworth <william10charles@gmail.com>
2017-04-04 11:35:56 -0600

    creating rediret functionality

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const { storeUrl, getUrls } = require("./link_shortener");
-const {objectToArray} = require("./helpers")
 
-
+const { storeUrl, getUrls, getUrl } = require("./link_shortener");
+const { objectToArray } = require("./helpers");
 
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+storeUrl("google.com");
+
 app.get("/", (req, res) => {
   getUrls().then(data => {
     data = objectToArray(data);
-    res.render('index', {data});
+    res.render("index", { data });
   });
 });
 
-app.post
+app.get("/:hash", (req, res) => {
+  let hash = req.params.hash;
+  getUrl(`localhost:3000/${hash}`).then(data => {
+    console.log(data);
+    res.send(`http://www.${data}`);
+  });
+});
 
 server.listen(3000);
--- link_shortener.js
+++ link_shortener.js
@@ -1,31 +1,42 @@
 const redisClient = require("redis").createClient();
 const sh = require("shorthash");
 
-function _shortenUrl (url) {
-	return `localhost:3000/${sh.unique(url)}`;
+function _shortenUrl(url) {
+  return sh.unique(url);
 }
 
 function storeUrl(url) {
-	let shortUrl = _shortenUrl(url);
-	redisClient.hsetnx('urlHash', shortUrl, url, (err, result) => {
-		if (err) {throw err;}
-		if (result) {
-			console.log("new field set");
-		} else {
-			console.log("field already exists");
-		}
-	})
+  let shortUrl = _shortenUrl(url);
+  redisClient.hsetnx("urlHash", shortUrl, url, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    if (result) {
+      console.log("new field set");
+    } else {
+      console.log("field already exists");
+    }
+  });
 }
 
 function getUrls() {
-	return new Promise((resolve) => {
-		redisClient.hgetall('urlHash', (err, data) => {
-			return resolve(data);
-		})
-	})
+  return new Promise(resolve => {
+    redisClient.hgetall("urlHash", (err, data) => {
+      return resolve(data);
+    });
+  });
+}
+
+function getUrl(hash) {
+  return new Promise(resolve => {
+    redisClient.hget("urlHash", hash, (err, data) => {
+      return resolve(data);
+    });
+  });
 }
 
 module.exports = {
-	storeUrl,
-	getUrls
-}
+  storeUrl,
+  getUrls,
+  getUrl
+};
