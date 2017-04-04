a361f00875ab60cc3f82bf5ad02456b297c408fa
Jerry Gao <gao@blackwright.com>
2017-04-04 13:39:29 -0400

    add get view

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var { linkShortener, shortenedLinkInfo } = require('../lib/linkShortener');
+var {
+  flush,
+  getKeys,
+  linkShortener,
+  shortenedLinkInfo
+} = require("../lib/linkShortener");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index');
+router.get("/", function(req, res, next) {
+  var p = getKeys();
+  const urlInfo = [];
+  p.then(data => {
+    data.forEach(key => {
+      var info = shortenedLinkInfo(key);
+      info.then(array => {
+        urlInfo.push({
+          shortened: key,
+          original: array[0],
+          count: array[1]
+        });
+      });
+    });
+    res.render("index", { urlInfo });
+  });
 });
 
-router.post('/submit', function(req, res, next) {
+router.post("/submit", function(req, res, next) {
   var url = req.body.url;
   var shortUrl = linkShortener(url);
   var p = shortenedLinkInfo(shortUrl);
   p.then(data => {
-    res.render('index', {shortUrl, data});
+    // res.render("index", { shortUrl, data });
+    res.redirect("/");
   });
-
-})
+});
 
 module.exports = router;
