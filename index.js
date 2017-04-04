b2c1e4f9b2819e43f7cb08a1062c0751bc2f94f8
Nicholas Romeo <romeonicholas@gmail.com>
2017-04-04 11:46:25 -0700

    Temporary array setup for demo

--- index.js
+++ index.js
@@ -20,23 +20,36 @@ app.use(express.static(__dirname + '/public'));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//redisClient.flushall();
+
 app.get('/', (req, res) => {
+  var urls = [];
+  var hashes = [];
 
-  let urls = [];
+  redisClient.keys('-*', (err, results) => {
+    hashes = results;
+    // redisClient.keys('*', (err, results) => {
 
-  redisClient.keys('*', (err, results) => {
-    urls = results;
+    hashes.forEach(function(value) {
+      redisClient.get(value, (err, results) => {
+        urls.push(results);
+        console.log(urls);
+      });
+    });
 
-    res.render('index', { urls });
+    res.render('index', { urls, hashes });
+
+    // });
+
     //console.log(results);
   });
   //Get redis keys, pass them through
   //  res.render('castles', { kingdomName, castles });
-  
 });
 
 app.get('/:hashcode', (req, res) => {
   let redirectURL = req.params.hashcode;
+
   redisClient.get(redirectURL, (err, results) => {
     console.log(results);
     res.redirect(`http://www.${results}`);
@@ -48,14 +61,21 @@ var redObj = {};
 app.post('/', (req, res) => {
   var userURL = req.body.userURL;
   var hashedURL = encode().value(req.body.userURL);
-  var redObj = { userURL: hashedURL};
-  // redisClient.set(userURL, hashedURL);
-  // redisClient.set(hashedURL, userURL);
-  redisClient.set(userURL, redObj)
-  redisClient.get(userURL, (err, results) => {
-    console.log(results);
-  });
+
+  //var redObj = { userURL: hashedURL };
+  redisClient.set(hashedURL, userURL);
+  redisClient.set(userURL, hashedURL);
+  res.redirect('back');
+
+  //redisClient.set(hashedURL, userURL);
+
+  //redisClient.set(userURL, redObj);
+
+  // redisClient.get(userURL, (err, results) => {
+  //   console.log(results);
+  // });
 });
+
 //
 // io.on('connection', client => {
 //   redisClient.get('count', (err, count) => {
