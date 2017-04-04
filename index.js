4d8dc03d6bb72be0aa76b694d6cd9b98b70605f1
markmarkyesyes <markhahn545@gmail.com>
2017-04-04 12:10:12 -0500

    implemented redis storage of url pairs (base url and short url) from input in the html form through post router in index.js

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ const express = require("express")
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const bodyParser = require('body-parser')
+const linkShortener = require('./lib/link_shortener')
 redisClient = require("redis").createClient();
 
 
 
 ///////////////////
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // redisClient.flushall();
 
 //console.log(shortid.generate());
 
 app.get('/', (req, res) => {
-  redisClient.get("count", (err, value) => {
-    console.log(value);
+    redisClient.get(urlPair.inputURL, (err, value) => {
+      console.log(value);
+    })
     res.sendFile(__dirname + "/index.html");
-  })
 });
 
 app.post('/update', (req, res) => {
-  console.log(req.body)
+  let inputURL = req.body.baseURL;
+  let urlPair = linkShortener(inputURL);
+
+  redisClient.setnx(urlPair.inputURL, urlPair.shortURL)
+
   res.redirect('/')
 })
 
