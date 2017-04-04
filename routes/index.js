0fcf0c9c8c9f7b2b269db37420597a71e4cd4e9e
Egle Libby <eglelibby@gmail.com>
2017-04-04 13:09:39 -0400

    shortened url

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var linkShortener = require('../lib/linkShortener');
+var { linkShortener, shortenedLinkInfo } = require('../lib/linkShortener');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var p = linkShortener('www.google.com');
-  p.then(data => {
     res.render('index');
-  });
 });
+
+router.post('/submit', function(req, res, next) {
+  var url = req.body.url;
+  var shortUrl = linkShortener(url);
+  var p = shortenedLinkInfo(shortUrl);
+  p.then(data => {
+    res.render('index', {shortUrl, data});
+  });
+
+})
 
 module.exports = router;
