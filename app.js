b3d22be8b0a8a275f60361781e7449d86a234b61
nicoasp <nicolas.amaya@gmail.com>
2017-04-04 18:23:11 +0200

    finish link-shortener module

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const linkShortener = require("./link_shortener");
+const {storeUrl, getUrls} = require("./link_shortener");
 
 //function takes url
 //hashes url
 //stores hash as key and url as value in redis
 //link_shortener will store and retieve
+
+//app.get('/', (req, res) => {
+	getUrls()
+	.then((data) => {
+		console.log(data);
+	});
+//})
--- link_shortener.js
+++ link_shortener.js
@@ -1,5 +1,31 @@
 const redisClient = require("redis").createClient();
 const sh = require("shorthash");
 
-var test = sh.unique("www.google.com");
-console.log(test);
+function _shortenUrl (url) {
+	return `localhost:3000/${sh.unique(url)}`;
+}
+
+function storeUrl(url) {
+	let shortUrl = _shortenUrl(url);
+	redisClient.hsetnx('urlHash', shortUrl, url, (err, result) => {
+		if (err) {throw err;}
+		if (result) {
+			console.log("new field set");
+		} else {
+			console.log("field already exists");
+		}
+	})
+}
+
+function getUrls() {
+	return new Promise((resolve) => {
+		redisClient.hgetall('urlHash', (err, data) => {
+			return resolve(data);
+		})
+	})
+}
+
+module.exports = {
+	storeUrl,
+	getUrls
+}
