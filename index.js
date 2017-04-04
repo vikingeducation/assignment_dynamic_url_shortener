993d67ee554a4d8ce40f3a974c2066a0cad7b90d
Nicholas Romeo <romeonicholas@gmail.com>
2017-04-04 10:37:21 -0700

    Break express

--- index.js
+++ index.js
@@ -5,28 +5,43 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const redisClient = require('redis').createClient();
-const hbs = require('express-handlebars');
+const expressHandlebars = require('express-handlebars');
 const encode = require('hashcode').hashCode;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'hbs');
 
-//redisClient.setnx('count', 0);
+const hbs = expressHandlebars.create({
+  defaultLayout: 'main'
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index');
+
+  let urls = [];
+
+  redisClient.keys('*', (err, results) => {
+    urls = results;
+    //console.log(results);
+  });
+  //Get redis keys, pass them through
+  //  res.render('castles', { kingdomName, castles });
+  res.render('index', { urls });
 });
 
 app.get('/:hashcode', (req, res) => {
-  //un-hash :hashcode, store as variable
-  //res.redirect(unhashed hashcode);
   let redirectURL = req.params.hashcode;
   redisClient.get(redirectURL, (err, results) => {
     console.log(results);
     res.redirect(`http://www.${results}`);
   });
 });
+
 app.post('/', (req, res) => {
   var userURL = req.body.userURL;
   var hashedURL = encode().value(req.body.userURL);
@@ -36,25 +51,24 @@ app.post('/', (req, res) => {
   redisClient.get(userURL, (err, results) => {
     console.log(results);
   });
-  
 });
-
-io.on('connection', client => {
-  redisClient.get('count', (err, count) => {
-    client.emit('new count', count);
-  });
-
-  client.on('increment', () => {
-    redisClient.incr('count', (err, count) => {
-      io.emit('new count', count);
-    });
-  });
-
-  client.on('decrement', () => {
-    redisClient.decr('count', (err, count) => {
-      io.emit('new count', count);
-    });
-  });
-});
+//
+// io.on('connection', client => {
+//   redisClient.get('count', (err, count) => {
+//     client.emit('new count', count);
+//   });
+//
+//   client.on('increment', () => {
+//     redisClient.incr('count', (err, count) => {
+//       io.emit('new count', count);
+//     });
+//   });
+//
+//   client.on('decrement', () => {
+//     redisClient.decr('count', (err, count) => {
+//       io.emit('new count', count);
+//     });
+//   });
+// });
 
 server.listen(3000);
