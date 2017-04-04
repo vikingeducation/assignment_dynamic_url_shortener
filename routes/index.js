c755f27de0a4f19f9f81d8a0211c6b68679b2afa
Egle Libby <eglelibby@gmail.com>
2017-04-04 12:20:40 -0400

    testing link shortener

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var linkShortener = require('../lib/linkShortener');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var array = linkShortener('www.google.com');
+  console.log(array);
   res.render('index', { title: 'Express' });
 });
 
