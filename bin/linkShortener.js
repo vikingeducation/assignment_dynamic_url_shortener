cc10f44910e26fa251f7a1960a4ae4738f44e45c
rabGIT <rabGIT@users.noreply.github.com>
2017-05-08 15:19:55 -0700

    add & display links

--- bin/linkShortener.js
+++ bin/linkShortener.js
@@ -1,19 +1,40 @@
 const redisClient = require("redis").createClient();
 const sh = require("shorthash");
+const debug = require('debug')('linkShorterner');
 
-
-let url = 'http://www.google.com/fasfd/fsdaf/ffd/f/23wresg/search/test124';
-
+redisClient.del("URLList");
 
 function shortenURL(url) {
   let shortURL = sh.unique(url);
-  redisClient.hmset(shortURL, {"shortURL": shortURL, "longURL": url, "count": "0"});
+  redisClient.hmset(shortURL, {
+    "shortURL": shortURL,
+    "longURL": url,
+    "count": "0"
+  });
+  redisClient.rpush("URLList", shortURL);
+  debug(`shortened URL ${url} to ${shortURL}`);
   return shortURL;
 }
 
-function lengthenURL(url, callback) {
-  redisClient.hgetall(url, callback);
+function getURLs() {
+  return new Promise(function(resolve, reject) {
+    redisClient.lrange("URLList", 0, -1, function(err, data) {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+
 }
+
+function lengthenURL(url) {
+  return new Promise(function(resolve, reject) {
+      redisClient.hgetall(url, function (err, obj) {
+        if (err) reject(err);
+        resolve(obj);
+      });
+    })
+  };
+
 
 function addClick(url) {
   redisClient.hincrby(url, "count", 1);
@@ -22,17 +43,11 @@ function addClick(url) {
 module.exports = {
   shortenURL,
   lengthenURL,
+  getURLs,
   addClick
 };
 
-/*
-let test = shortenURL(url, redisClient.print);
-console.log(test);
-lengthenURL(test, function(err, obj) {
-  console.log(obj);
-});
-addClick(test);
-lengthenURL(test, function(err, obj) {
-  console.log(obj);
-});
-*/
+
+let url = 'http://www.google.com/';
+
+let test = shortenURL(url);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const ls = require('../bin/linkShortener.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let urls = [];
+  ls.getURLs().then(function(data) {
+    data.forEach(function(shortURL) {
+      urls.push(ls.lengthenURL(shortURL));
+    })
+    Promise.all(urls).then(function(urlArray) {
+      res.render('index', {
+        title: 'Link Shortener',
+        server: 'http://localhost:3000',
+        urls: urlArray
+      });
+    });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  ls.shortenURL(req.body.originalURL);
+  res.redirect("back");
 });
 
 module.exports = router;
