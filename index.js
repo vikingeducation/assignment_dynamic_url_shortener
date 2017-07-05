40662ba2ab9960d6d98fddc0282b1de7914999fb
Malyar Baron <malbaron0@gmail.com>
2017-07-05 00:21:26 -0400

    Implementation of redis
    Storing URL as key with shortened url and count as values in redis.

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    //Testing the linkShortner module below.
+
     linkShortner.storeUrl('www.ign.com');
 
-    const count = linkShortner.getCount('www.google.com');
-    const url = linkShortner.getUrl('www.google.com');
-    
-    linkShortner.getAll();
+    //const count = linkShortner.getCount('www.google.com');
+    linkShortner.getUrlObj('www.google.com');
+    //linkShortner.incrCount('www.google.com');
 
-    console.log(`\n ${count} ${url}`);
+    //linkShortner.getAll();
+
+    //console.log(`\n ${count} ${url}`);
     res.end()
 });
 
--- modules/link-shortener.js
+++ modules/link-shortener.js
@@ -1,9 +1,5 @@
-ShortURL = {
-    'www.google.com': {
-        shortUrl: "shortUrl",
-        count: 0
-    }
-}
+var redis = require('redis');
+var client = redis.createClient(); //creates a new client
 
 const randomString = function () {
     let text = "";
@@ -20,19 +16,31 @@ const linkShortner = {
         const randomText = randomString();
         ShortURL[fullURL] = {};
 
-        ShortURL[fullURL]['shortUrl'] = `ragnar/${randomText}`;
-        ShortURL[fullURL]['count'] = 0;
+        //Check if key already exists, for now only logging to console. But maybe return a boolean Value and show message on the html page
+        client.exists(fullURL, (err, reply) => {
+            if (reply === 1) {
+                console.log("URL Already Exists!!");
+            }
+            else {
+                //add to redis the new URL key
+                client.hmset(fullURL, "shortUrl", `ragnar/${randomText}`, "count", 0);
+            }
+        });
+
     },
 
-
-    getUrl: (URL) => {
-        return ShortURL[URL].shortUrl
+    //Just get the URL object/hash from redis
+    getUrlObj: (URL) => {
+        client.hgetall(URL, function (err, obj) {
+            console.log(obj);
+        });
     },
 
     getCount: (URL) => {
-        return ShortURL[URL].count;
+        client.hgetall(URL, function (err, obj) {
+            console.log(obj.count);
+        });
     },
-
 
     incrCount: (URL) => {
         let count = ShortURL[URL].count++;
