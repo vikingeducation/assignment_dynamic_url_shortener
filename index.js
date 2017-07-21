735d6a7834b59d6a472b4ef5ef8abde6eb9d0cd0
Tyler Ketron <tketron@gmail.com>
2017-07-20 17:01:11 -0700

    promisify link-shortener module and add original url and shortened url into html table

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 const {
   setShortenedLink,
-  getShortenedLink
+  getShortenedLink,
+  getAllURLs
 } = require('./link-shortener');
+
+const app = express();
+
+const hbs = expressHandlebars.create({
+  defaultLayout: 'main'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  // let urls = getAllURLs();
+  // console.log(urls);
+  // res.render('index', { urls });
+  getAllURLs().then(urls => {
+    console.log(urls);
+    res.render('index', { urls: urls });
+  });
+});
+
+app.post('/', (req, res) => {
+  let url = req.body.link;
+  setShortenedLink(url);
+  res.redirect('back');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
--- link-shortener.js
+++ link-shortener.js
@@ -1,46 +1,70 @@
 const redis = require('redis');
 const redisClient = redis.createClient();
 
-const setShortenedLink = (url) => {
+const setShortenedLink = url => {
   let shortenedLink = shortenLink(url);
-  redisClient.set(url, shortenedLink, redis.print);
+  // redisClient.set(url, shortenedLink, redis.print);
+  redisClient.hset('urls', url, shortenedLink);
   // console.log(url + ", " + shortenedLink);
 };
 
 //promise based implementation
-const getShortenedLink = (url) => {
+const getShortenedLink = url => {
   return new Promise((resolve, reject) => {
-    redisClient.get(url, (err, value) => {
+    redisClient.hget('urls', url, (err, value) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(value)
+        resolve(value);
+      }
+    });
+  });
+};
+
+const getAllURLs = () => {
+  return new Promise((resolve, reject) => {
+    redisClient.hgetall('urls', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        // console.log(data);
+        resolve(data);
       }
     });
   });
 };
 
 //accepts url and returns abbreviated url
-const shortenLink = (url) => {
-  var shortenedURL = url.split("").filter( (char, index) => {
+const shortenLink = url => {
+  var shortenedURL = url.split('').filter((char, index) => {
     if (index % 3 === 0) {
       return char;
     }
   });
-  return shortenedURL.join("");
-}
+  return shortenedURL.join('');
+};
 
-var url = "googlefacebookmyspace";
-var fakeUrl = "notinthedatabaseyet"
-console.log("base url: " + url);
-setShortenedLink(url);
-getShortenedLink(fakeUrl).then( value => {
-  console.log(value);
-}).catch(err => {
-  console.err(err);
-});
+// var url = 'googlefacebookmyspace';
+// var fakeUrl = 'notinthedatabaseyet';
+// setShortenedLink(url);
+// setShortenedLink(fakeUrl);
+// getShortenedLink(fakeUrl)
+//   .then(value => {
+//     console.log(value);
+//   })
+//   .catch(err => {
+//     console.err(err);
+//   });
+// getAllURLs();
+// .then(value => {
+//   console.log(value);
+// })
+// .catch(err => {
+//   console.err(err);
+// });
 
 module.exports = {
   setShortenedLink,
-  getShortenedLink
+  getShortenedLink,
+  getAllURLs
 };
