9aef2dc5d557782e0e97a327a76f88e6155e1f56
Kara Thrash <dvdonato@gmail.com>
2017-07-25 12:01:04 -0400

    external url works

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 var shortid = require("shortid");
-console.log(shortid);
+var express = require("express");
+var app = express();
+//var urlShortner = require("./lib/urlshortener");
+
+var port = 4000;
+var host = "localhost";
+
+var savedURL = { test: "https://www.google.com/" };
+
+app.listen(port, () => {
+  console.log("Serving!" + port);
+});
+
+app.get("/", (req, res) => {
+  console.log(req.url);
+});
+app.get("/key/:shortUrl", (req, res) => {
+  //console.log(req);
+  if (savedURL[req.params.shortUrl] != undefined) {
+    res.redirect(savedURL[req.params.shortUrl]);
+    console.log(savedURL[req.params.shortUrl]);
+  } else {
+    console.log("not saved");
+  }
+});
+//app.post(, (req, res) => {}););
