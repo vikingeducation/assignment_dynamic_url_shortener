912d28748731678402c05e34149bab06a938d9b8
Andrew Senner <me@andrewsenner.com>
2017-07-25 13:12:05 -0400

    Redis wrapper working, shortener working, basic UI implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const shorten = require('../lib/link_shortener');
+const redisWrapper = require('../lib/redis_wrapper');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Dynamic URL Shortener' });
+	// Fetch all urls from the database.
+	redisWrapper.getAllLinks().then(urlObject => {
+		// Convert urls into array of objects.
+		let urls = [];
+		for (let long in urlObject) {
+			let short = urlObject[long];
+			urls.push({
+				short: short,
+				long: long
+			});
+		}
+		console.log(urls);
+
+		res.render('index', {
+			title: 'Dynamic URL Shortener',
+			urls: urls
+		});
+	});
+	//res.status(200).end();
 });
 
 module.exports = router;
