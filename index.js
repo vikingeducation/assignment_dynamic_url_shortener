aaf24edfac5bc2995b8d645e414058251493e4d2
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-25 14:00:48 -0400

    handlebars

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
 const router = require('./routes');
 const bodyParser = require('body-parser');
+const handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
