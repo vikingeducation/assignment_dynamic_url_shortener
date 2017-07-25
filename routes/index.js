d7055821c8d42fda40db7600ec398f1e2cdcef7f
Andrew Senner <me@andrewsenner.com>
2017-07-25 16:37:22 -0400

    Tomato

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,41 +8,59 @@ const shorten = require('../lib/link_shortener');
 const redisWrapper = require('../lib/redis_wrapper');
 
 /* GET home page. */
+const PAGE_TITLE = 'Simplify your links';
 router.get('/', function(req, res, next) {
-
 	// Fetch all urls from the database.
-	redisWrapper.getAllLinks().then(_parseLinks).then((links)=> {
-		res.render('index', {
-			title: 'Dynamic URL Shortener',
-			urls: links
+	redisWrapper.getAllLinks().then(_parseLinks).then(links => {
+		let countObj = redisWrapper.getUrlCounts().then(counts => {
+			links = links.map(urlObj => {
+				urlObj.count = counts[urlObj.long];
+				return urlObj;
+			});
+
+			res.render('index', {
+				title: PAGE_TITLE,
+				urls: links
+			});
 		});
 	});
 });
 
-router.get('/*', (req, res) => {
+router.get('/:shortId', (req, res) => {
 	// Pull params out for routing.
-	let path = url.parse(req.path).pathname.slice(1);
-	console.log(path)
-	if (path.length > 0) {
-		redisWrapper.loadLink(path).then((result)=>{
-			console.log(result)
-			if (result !== 'Not Found'){
-				res.redirect(result)
-			}
-		})
+	let urlId = req.params.shortId;
+	if (/^favicon/gi.test(urlId)) {
+		// This started giving us trouble
+		// out of the blue for no reason... TODO: Figure this out.
+		res.end();
+	} else {
+		if (urlId.length > 0) {
+			redisWrapper.loadLink(urlId).then(result => {
+				if (result !== 'Not Found') {
+					redisWrapper.incrementUrlCount(result).then(count => {
+						res.redirect(result); // TODO: Fix relative paths
+					});
+				} else {
+					res.render('error');
+				}
+			});
+		} else {
+			res.render('error');
+		}
 	}
-})
+});
 
+// Convert urls into array of objects.
 function _parseLinks(urlObject) {
-	// Convert urls into array of objects.
 	let urls = [];
-	for (let long in urlObject) {
-		let short = urlObject[long];
+	for (let short in urlObject) {
+		let long = urlObject[short];
 		urls.push({
 			short: short,
 			long: long
 		});
 	}
+	return urls;
 }
 
 module.exports = router;
