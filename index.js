0fc84609b62a93684f78105a580f360cf6a45721
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-25 14:40:11 -0400

    handlebars2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const app = require('express')();
 const router = require('./routes');
 const bodyParser = require('body-parser');
-const handlebars = require('handlebars')
+//const handlebars = require('handlebars')
 const exphbs = require('express-handlebars')
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view-engine', 'handlebars');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
