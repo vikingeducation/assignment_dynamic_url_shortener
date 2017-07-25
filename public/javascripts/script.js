876136d5e49cbdc59503cc2182078dec06deefad
Andrew Senner <me@andrewsenner.com>
2017-07-25 18:24:21 -0400

    socket working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-  var socket = io.connect("http://localhost:3000");
+	var socket = io.connect('http://localhost:3000');
 
-  socket.on("new_count", countObj => {
-    console.log(countObj);
-  });
+	socket.on('new_count', urlObj => {
+		console.log(urlObj);
+		console.log(`New count: ${urlObj.count}`);
+	});
 
-  // $("a").on("click", function(e) {
-  //   let $target = $(e.target);
-  //   let id = $target.attr("id");
-  //   socket.emit("increment", id);
-  // });
+	$('a').on('click', function(e) {
+		let $target = $(e.target).parents('a');
+		let shortUrl = $target.attr('id');
+		let longUrl = $target.data('long');
+	});
 });
