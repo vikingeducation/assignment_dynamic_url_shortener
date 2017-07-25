ffb03d7c22961ea83c4d86fa9bcc84fa1bb3642e
Stephanie Barker <song_mage@yahoo.com>
2017-07-25 12:32:49 -0400

    working on connecting express app and redis

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var shortid = require("shortid");
 var express = require("express");
+var bodyParser = require("body-parser");
+var hbs = require("express-handlebars");
+var urlShortener = require("./lib/urlshortener");
+
 var app = express();
-//var urlShortner = require("./lib/urlshortener");
 
 var port = 4000;
 var host = "localhost";
@@ -12,11 +15,16 @@ app.listen(port, () => {
   console.log("Serving!" + port);
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get("/", (req, res) => {
   console.log(req.url);
+  res.render("index")
 });
 app.get("/key/:shortUrl", (req, res) => {
-  //console.log(req);
   if (savedURL[req.params.shortUrl] != undefined) {
     res.redirect(savedURL[req.params.shortUrl]);
     console.log(savedURL[req.params.shortUrl]);
@@ -24,4 +32,10 @@ app.get("/key/:shortUrl", (req, res) => {
     console.log("not saved");
   }
 });
-//app.post(, (req, res) => {}););
+
+app.post("/", (req, res) => {
+	urlShortener.shortenURL(req.body.urlToShorten, (url) => {
+		console.log(url);
+		res.redirect("/");
+	});
+});
