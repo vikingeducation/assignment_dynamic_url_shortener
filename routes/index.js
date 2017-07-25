4056eea01b4a2715a2645e832769fd84e40f53f6
Andrew Senner <me@andrewsenner.com>
2017-07-25 13:43:26 -0400

    Fixed redis wrapper, started routing functionality for short urls

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const url = require('url');
 const shorten = require('../lib/link_shortener');
 const redisWrapper = require('../lib/redis_wrapper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// Fetch all urls from the database.
-	redisWrapper.getAllLinks().then(urlObject => {
-		// Convert urls into array of objects.
-		let urls = [];
-		for (let long in urlObject) {
-			let short = urlObject[long];
-			urls.push({
-				short: short,
-				long: long
-			});
-		}
-		console.log(urls);
+	// Pull params out for routing.
+	let path = url.parse(req.path).pathname.slice(1);
+	if (path.length > 0) {
+		// Fetch all urls from the database.
+		redisWrapper.getAllLinks().then(_parseLinks);
+	} else {
+	}
+});
+
+function _parseLinks(urlObject) {
+	// Convert urls into array of objects.
+	let urls = [];
+	for (let long in urlObject) {
+		let short = urlObject[long];
+		urls.push({
+			short: short,
+			long: long
+		});
 
 		res.render('index', {
 			title: 'Dynamic URL Shortener',
 			urls: urls
 		});
-	});
-	//res.status(200).end();
-});
+	}
+}
 
 module.exports = router;
