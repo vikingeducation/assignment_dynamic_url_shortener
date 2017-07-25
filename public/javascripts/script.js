4c869eba6946af7147601d1eca1e4b3ab6eabd6f
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-25 22:25:19 +0000

    tomato

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-	var socket = io.connect('http://localhost:3000');
+  var socket = io.connect("http://localhost:3030");
+  const template = $("a#template");
 
-	socket.on('new_count', urlObj => {
-		console.log(urlObj);
-		console.log(`New count: ${urlObj.count}`);
-	});
+  socket.on("new_count", urlObj => {
+    let anchor = $(`a#${urlObj.short}`);
+    console.log(anchor);
+    $(`a#${urlObj.short}`).find("span").text(urlObj.count);
+  });
 
-	$('a').on('click', function(e) {
-		let $target = $(e.target).parents('a');
-		let shortUrl = $target.attr('id');
-		let longUrl = $target.data('long');
-	});
+  $("a").on("click", function(e) {
+    let $target = $(e.target).parents("a");
+    let shortUrl = $target.attr("id");
+    let longUrl = $target.data("long");
+    e.preventDefault();
+  });
 });
