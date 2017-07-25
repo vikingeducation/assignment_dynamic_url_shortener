34dc9a30ef6e18686f7a77d0395a044cb0a76af4
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-25 12:04:24 -0500

    tt

--- index.js
+++ index.js
@@ -5,19 +5,34 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const redisClient = require("redis").createClient();
 const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
-lib.shortener.linkShortener();
 
 app.engine("handlebars", hbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static(`${__dirname}/public`));
 app.use("/socket.io", express.static(__dirname + "node_modules/socket.io-client/dist/"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   io.emit("anEvent");
   res.render("index");
 })
+
+
+app.post("/", (req, res) => {
+  let url = req.body.url;
+  let id = lib.shortener.linkShortener();
+  redisClient.set(id, url, () => {
+    console.log("This is working!");
+    res.render("index", { id, url});
+  })
+})
+
+
+
 
 // client.on("incr", (count) => {
 //
--- public/js/socket.js
+++ public/js/socket.js
@@ -6,4 +6,6 @@ var socket = io.connect('http://localhost:3000');
 
 socket.emit('event');
 
-
+$(".submission").on("click", ".makeUrl", () => {
+  alert("This is working");
+})
