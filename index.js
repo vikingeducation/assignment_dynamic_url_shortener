6000393335677754bb7ef0df267579efc9bf7736
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-25 20:21:20 -0500

    ffj

--- index.js
+++ index.js
@@ -8,40 +8,36 @@ const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const validUrl = require("valid-url");
 
-app.engine("handlebars", hbs({ defaultLayout: "main" }));
+app.engine("handlebars", hbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-
 app.use(express.static(`${__dirname}/public`));
 app.use("/socket.io", express.static(__dirname + "node_modules/socket.io-client/dist/"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.render("index");
 })
 
 function update(client) {
-  lib.redisTools.getAllKeys()
-    .then(keys => {
-      keys.forEach(key => {
-        lib.redisTools.getData(key)
-          .then(data => {
-            console.log("fjfjf", data);
-             client.emit("newId", data);
-          })
+  console.log("updating");
+  lib.redisTools.getAllKeys().then(keys => {
+    keys.forEach(key => {
+      lib.redisTools.getData(key).then(data => {
+        client.emit("newId", data);
       })
     })
+  })
 }
 
 let baseUrl = process.env.BASE_URL || "http://localhost:3000";
 
 io.on('connection', client => {
-	update(client);
+  update(client);
 
   client.on("url", (url) => {
 
     let id = lib.shortener.linkShortener();
-    let { name } = url;
+    let {name} = url;
 
     let objStorage = {
       id: id,
@@ -49,38 +45,31 @@ io.on('connection', client => {
       urlShort: `${baseUrl}/${id}`,
       count: 0
     }
-
+    
     if (validUrl.isUri(name)) {
-      lib.redisTools.storeData(id, objStorage)
-        .then(() => lib.redisTools.getData(id))
-        .then((data) => {
-          client.emit("newId", data);
-        })
+      lib.redisTools.storeData(id, objStorage).then(() => lib.redisTools.getData(id)).then((data) => {
+        client.emit("newId", data);
+      })
 
     } else {
       client.emit("inputError");
     }
   })
-})
 
-app.all('/:id', (req, res) => {
-  const id = req.params.id.trim();
-  console.log(`id: ${id}`);
-  lib.redisTools.getData(id)
-  .then((data) => {
-    data["count"] = (parseInt(data["count"]) + 1).toString();
-
-    lib.redisTools.storeData(data["id"], data)
-    .then(() => {
+  app.all('/:id', (req, res) => {
+    const id = req.params.id.trim();
+    lib.redisTools.getData(id)
+    .then((data) => {
+      data["count"] = (parseInt(data["count"]) + 1).toString();
+      lib.redisTools.storeData(data["id"], data)
+    .then(() => lib.redisTools.getData(id))
+    .then((data) => {
       console.log(data);
-      socket.emit("count", data);
-    })
-    res.redirect(data["urlLong"])
-
+      client.emit("count", data);
+      res.redirect(data["urlLong"])
+      })
+    }).catch((err) => console.log(err))
   })
-  .catch((err) => console.log(err))
 })
 
-server.listen(3000, () => {
-  console.log("I'm listening");
-});
+server.listen(3000);
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,38 +1,45 @@
-var socket = io.connect('http://localhost:3000');
 
-// socket.on('greeting', function(data) {
-//   console.log("acknowledged?")
-// })
+$(document).ready(() => {
+  const socket = io.connect('http://localhost:3000');
 
-socket.emit('event');
+  socket.emit('event');
 
-$(".submission").on("click", ".makeUrl", (e) => {
-  e.preventDefault();
-  const url = $(".urlSpecial").val();
-  socket.emit("url", { name: url });
+  $(".submission").on("click", ".makeUrl", (e) => {
+    e.preventDefault();
+    const url = $(".urlSpecial").val();
+    socket.emit("url", { name: url });
+  })
+
+  socket.on("newId", (data) => {
+    if( $(`#${data.id}`).length > 0) {
+      updateCount(data);
+      console.log("updating");
+    } else {
+      addTableData(data);
+    }
+
+  })
+
+  socket.on("inputError", () => {
+  	alert('Invalid Input!!! :(')
+  })
+
+  socket.on("count", (data) => {
+    console.log("marry poppins");
+    updateCount(data);
+  })
+
+  function addTableData(data) {
+  	$("tbody").append(
+  	  $(`<tr id=${data.id}>
+  	    <td><a href=${data.urlLong}>${data.urlLong}</a></td>
+  	    <td><a href=${data.urlShort}>${data.urlShort}</a></td>
+  	    <td class=${data.id}>${data.count}</td>
+  	  </tr>`))
+  }
+  //
+
+  function updateCount(data) {
+    $(`.${data.id}`).text(data.count);
+  }
 })
-
-socket.on("newId", (data) => {
-	addTableData(data);
-})
-
-socket.on("inputError", () => {
-	alert('Invalid Input!!! :(')
-})
-
-socket.on("count", (data) => {
-  increment(data);
-})
-
-function addTableData(data) {
-	$("tbody").append(
-	  $(`<tr>
-	    <td><a href=${data.urlLong}>${data.urlLong}</a></td>
-	    <td><a href=${data.urlShort}>${data.urlShort}</a></td>
-	    <td class=${data.id}>${data.count}</td>
-	  </tr>`))
-}
-
-function increment(data) {
-  $(`.${data.id}`).text(data.count);
-}
