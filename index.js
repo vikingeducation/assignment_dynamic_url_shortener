67871bd8d82fc3574a639001026a8ca70e634a60
Chuck Michael <chuckqs@yahoo.com>
2017-08-30 12:30:30 -0400

    added Socketio to project

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const io = require("socket.io")(server);
 const linkShortner = require("./modules/link-shortner");
 const shortId = require("shortid");
 
+var getIndex = function (list, value) {
+  var index = 0;
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] === value) {
+      index = i;
+    }
+  }
+  return index;
+}
+
 app.use(
   "/socket.io",
   express.static(__dirname + "node_modules/socket.io-client/dist/")
@@ -37,7 +47,7 @@ app.get("/", (req, res) => {
 app.get("/newUrl", (req, res) => {
   const sentUrl = req.url.slice(19);
   const shortUrl = shortId.generate();
-  linkShortner.setSite(sentUrl, "/sUrl/" + shortUrl);
+  linkShortner.setSite(sentUrl,shortUrl);
   res.redirect("back");
 })
 
@@ -50,19 +60,31 @@ app.get("/sUrl/:shortName", (req, res) => {
     let fullSiteList = values[0]
     let shortSiteList = values[1]
     let clickSiteList = values[2]
-    var index = 0;
-    console.log(shortSiteList)
-    for (var i = 0; i < shortSiteList.length; i++) {
-      if (shortSiteList[i] === ("/sUrl/" + req.params.shortName)) {
-        index = i;
-      }
-    }
 
-    //increase count
-    linkShortner.increaseClickSite(index, clickSiteList[index]);
+    const index = getIndex(shortSiteList, (req.params.shortName));
+
     res.redirect("http://" + fullSiteList[index])
   })
 
 })
+
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on("newClick", function (data) {
+      Promise.resolve(
+       linkShortner.getShortenSites()
+      ).then( (values) => {
+        const index = getIndex(values[0], data.classOfTarget);
+        //increase count
+        linkShortner.increaseClickSite(index, data.countTarget);
+        io.emit('newCount', {data})
+      })
+
+    });
+  });
+
 
 server.listen(3000);
