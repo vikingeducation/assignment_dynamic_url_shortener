3765ca03bc305d3a23bbd23a5e26b0a5d6e70021
DC <heyyodc-git@yahoo.com>
2017-11-03 13:54:04 -0700

    wrote redis wrapper. chose good redis data structure. need to resolve nested each in handlebars

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # assignment_dynamic_url_shortener
 Build a URL shortening service, outfitted with realtime analytics.
+
+Usage
+-----
+From command-line: 
+```
+node index.js
+```
+
+#### by Dennis C :coffee:
--- indexd.js
+++ indexd.js
@@ -3,16 +3,54 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
-const port = 4000;
+const bodyParser = require("body-parser");
+const randomstring = require("randomstring");
+const redis = require("./lib/redis-lib");
+const port = 4005;
 
+//body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//setup handlebars
 app.set("views", __dirname + "/views");
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.engine("handlebars", handlebars({ defaultLayout: "maind" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-	res.render("results", { something: "my content!" }); // render() for views, sendFile() for not
+	res.render("results", { test: "test content" }); // render() for views, sendFile() for not
+});
+
+app.post("/update", (req, res) => {
+	let originalUrl = req.body.formOrigUrl; // console.log(originalUrl);
+	let shortUrl = randomstring.generate({ length: 5, charset: "alphabetic" });
+	let urls,
+		counts = [];
+
+	redis
+		.writeUrl(originalUrl, shortUrl)
+		.then(data => {
+			if (data) console.log("wrote OK");
+			return redis.initCount(shortUrl);
+		})
+		.then(data => {
+			if (data) console.log("init count OK");
+			return redis.getUrls();
+		})
+		.then(data => {
+			urls = data;
+			return redis.getCounts();
+		})
+		.then(data => {
+			counts = data;
+			console.log(counts);
+			console.log(urls); // res.redirect("back");
+			res.render("results", { myCounts: counts, myUrls: urls });
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 app.listen(port, () => {
