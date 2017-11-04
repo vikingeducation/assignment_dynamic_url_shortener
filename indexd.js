1bc8b0c4d2f3e174b3567a30c392073d3190ccef
DC <heyyodc-git@yahoo.com>
2017-11-03 18:20:00 -0700

    counts increment on all redirects. refactored. solid

--- indexd.js
+++ indexd.js
@@ -6,7 +6,11 @@ const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
 const randomstring = require("randomstring");
 const redis = require("./lib/redis-lib");
-const port = 4000;
+
+const APP_URL_KEY = "urlKey";
+const APP_CLICK_KEY = "clickKey";
+const SHORT_URL_SIZE = 5;
+const PORT = 4000;
 
 //body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +29,11 @@ app.get("/", (req, res) => {
 app.get("/:shorturl", (req, res) => {
 	let shortUrl = req.params.shorturl;
 	redis
-		.getOriginalUrl(shortUrl)
+		.incrementCount(APP_CLICK_KEY, shortUrl)
+		.then(data => {
+			console.log(`count incremented to: ${data}`);
+			return redis.getOriginalUrl(APP_URL_KEY, shortUrl);
+		})
 		.then(data => {
 			let link = data;
 			res.redirect(link);
@@ -37,29 +45,29 @@ app.get("/:shorturl", (req, res) => {
 
 app.post("/update", (req, res) => {
 	let originalUrl = req.body.formOrigUrl; // console.log(originalUrl);
-	let shortUrl = randomstring.generate({ length: 5, charset: "alphabetic" });
+	let shortUrl = randomstring.generate({
+		length: SHORT_URL_SIZE,
+		charset: "alphabetic"
+	});
 	let urls,
 		counts = [];
 
 	redis
-		.writeUrl(originalUrl, shortUrl)
+		.writeUrl(APP_URL_KEY, originalUrl, shortUrl)
 		.then(data => {
 			if (data) console.log("wrote OK");
-			return redis.initCount(shortUrl);
+			return redis.initCount(APP_CLICK_KEY, shortUrl);
 		})
 		.then(data => {
 			if (data) console.log("init count OK");
-			return redis.getUrls();
+			return redis.getUrls(APP_URL_KEY);
 		})
 		.then(data => {
 			urls = data;
-			return redis.getCounts();
+			return redis.getCounts(APP_CLICK_KEY);
 		})
 		.then(data => {
 			let host = req.get("host");
-			console.log(">>>");
-			console.log(host);
-			console.log("<<<");
 			counts = data;
 			// console.log(counts); // console.log(urls); // res.redirect("back");
 			res.render("results", {
@@ -73,6 +81,6 @@ app.post("/update", (req, res) => {
 		});
 });
 
-app.listen(port, () => {
-	console.log(`listening on localhost:${port}`);
+app.listen(PORT, () => {
+	console.log(`listening on localhost:${PORT}`);
 });
