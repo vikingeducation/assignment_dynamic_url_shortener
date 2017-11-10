c7d45935a59166753c66112cda3e2cd76f68bee3
DC <heyyodc-git@yahoo.com>
2017-11-09 16:07:54 -0800

    added note about google chrome bug that inserts favicon.ico into route

--- index.js
+++ index.js
@@ -20,24 +20,41 @@ app.set("views", __dirname + "/views");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
-app.use(bodyParser.urlencoded({ extended: false }));
+//setup middleware
+app.use(bodyParser.urlencoded({ extended: false })); //body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
 app.use(express.static(__dirname + "/public"));
-app.use(
-	"/socket.io", // DC: this is saying use socket.io.js file in below path (I think)
-	express.static(__dirname + "node_modules/socket.io-client/dist/")
-);
+
+// this code may not be useful
+// see here for app.use() usage: https://expressjs.com/en/api.html#path-examples
+// app.use(
+// 	"/socket.io",
+// 	express.static(__dirname + "node_modules/socket.io-client/dist/")
+// );
+
+// handle google chrome favicon.ico bug (currently using chrome 62)
+// google chrome browser is bugily passing favicon.ico to my route causing program to crash
+// bug desc here: https://bugs.chromium.org/p/chromium/issues/detail?id=39402
+// and here: https://stackoverflow.com/questions/1003350/why-is-chrome-searching-for-my-favicon-ico-when-i-serve-up-a-file-from-asp-net-m
+// potential solution here: https://stackoverflow.com/questions/27117337/exclude-route-from-express-middleware
+// note: no issues on firefox 56, edge 40
+// conclusion: use non-chrome browser for now as I am not attempting to resolve this bug at the moment
+// DC, 2017-11-09
 
 //register routes
 app.get("/", (req, res) => {
-	res.render("results", { test: "test content" }); // render() for views, sendFile() for not
+	res.render("results", { test: "test content" }); // render() for views, sendFile() for static files
 });
 
+//route for when user consumes short url link
 app.get("/:shorturl", (req, res) => {
-	io.on("connection", () => {
+	io.on("connection", socket => {
 		console.log("a user connected at /:shorturl");
+		socket.on("disconnect", () => {
+			console.log("--> disconnected at /:shorturl");
+		});
 	});
 	let shortUrl = req.params.shorturl;
+	console.log("DC: shortUrl = " + shortUrl);
 	redis
 		.incrementCount(APP_CLICK_KEY, shortUrl)
 		.then(data => {
@@ -53,9 +70,13 @@ app.get("/:shorturl", (req, res) => {
 		});
 });
 
+//route for when user submits original long url
 app.post("/update", (req, res) => {
-	io.on("connection", () => {
+	io.on("connection", socket => {
 		console.log("a user connected at /update");
+		socket.on("disconnect", () => {
+			console.log("--> disconnected at /update");
+		});
 	});
 	let originalUrl = req.body.formOrigUrl; // console.log(originalUrl);
 	let shortUrl = randomstring.generate({
@@ -94,8 +115,11 @@ app.post("/update", (req, res) => {
 		});
 });
 
-io.on("connection", () => {
+io.on("connection", socket => {
 	console.log("a user connected");
+	socket.on("disconnect", () => {
+		console.log("--> disconnected");
+	});
 });
 
 //when using socket.io make sure you change app --> server
