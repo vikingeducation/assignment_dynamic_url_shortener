da28e58f6647e3aefed650125dcd08f61fdd843f
Hai Nguyen <memyselfandhai@gmail.com>
2017-11-21 11:01:29 -0600

    redis added

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const server = require("http").createServer(app);
 // socket server?
 const io = require("socket.io")(server);
 const exphbs = require("express-handlebars");
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }))
-const redisClient = redis.createClient();
-
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+const redisClient = require("redis").createClient();
+const url = require("short-url");
 
 app.use(
   "/socket.io",
@@ -19,15 +18,20 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.render('main');
-})
+  res.render("main");
+});
 
-app.listen(3000, 'localhost', () => {
+app.listen(3000, "localhost", () => {});
 
-})
+app.post("/", (req, res) => {
+  let long_url = req.body["url-input"];
 
-app.post('/', (req, res) => {
-  let url = req.body["url-input"];
-  console.log(req.body['url-input']);
-  res.redirect('back');
-})
+  url.shorten("www.google.com", function(err, short_url) {
+    // redisClient.setnx(long_url, short_url);
+    // redisClient.get(long_url, (err, short) => {
+    console.log(short_url);
+
+    // });
+    res.redirect("/");
+  });
+});
